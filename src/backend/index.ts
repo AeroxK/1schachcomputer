064f7e0171f298b8b8243fe707dbd15e251f6739
Alexander König <alexander.koenig@namics.com>
2021-02-24 01:28:56 +0100

    Dev convenience changes

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express';
 import { GAME_API_URL, MOVE_API_URL } from '../shared/config';
 import * as FenParser from './FenParser/fen-parser';
 import { getLegalSquaresForPiece, makeMove } from './ChessRules/chess-rules';
+import { ChessGame } from '../shared/types';
 
 const app = express();
 const port = 3000;
@@ -10,9 +11,10 @@ const port = 3000;
 app.use(express.json());
 app.use(express.static('dist/assets'));
 
-let game = FenParser.read('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
+let game: ChessGame;
 
 app.get(GAME_API_URL, (req: Request, res: Response) => {
+    game = FenParser.read('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
     res.send(game.board);
 });
 
