53729765ff7f1cadc80d4fd93ee479bec6f083fc
Alexander König <alexander.koenig@namics.com>
2021-03-06 12:35:50 +0100

    8: Add basic login page contents

--- src/frontend/LoginPage/LoginPage.tsx
+++ src/frontend/LoginPage/LoginPage.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import { RoutePaths } from '../shared/config';
 
-export default class LoginPage extends React.Component {
-    render() {
-        return (
-            <Link to={ RoutePaths.ChessGamePage }>Play!</Link>
-        )
-    }
+import { makeStyles } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    buttonWrapper: {
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        width: '100vw',
+        height: '100vh'
+    },
+});
+
+export default function LoginPage() {
+    const classes = useStyles();
+    return (
+        <Box bgcolor="primary.main" color="primary.contrastText">
+            <div className={classes.buttonWrapper}>
+                <Typography gutterBottom variant="h5" component="h1">
+                    1schachcomputer
+                </Typography>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    href={ RoutePaths.ChessGamePage }
+                >
+                    Login
+                </Button>
+            </div>
+        </Box>
+    );
 }
